482a39439b11ff3d024721dd3a9553ad2f9a26b5
musabbihanoor <musabbihanoor33@gmail.com>
2022-06-20 04:37:31 +0500

    show details and text editor seperately in write story and books

--- src/components/Story/WriteBook.js
+++ src/components/Story/WriteBook.js
@@ -9,17 +9,7 @@ import draftToHtml from "draftjs-to-html";
 import { convertToRaw } from "draft-js";
 
 const WriteBook = ({ close }) => {
-  const getBase64 = (file, cb) => {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-      cb(reader.result);
-    };
-    reader.onerror = function (error) {
-      console.log("Error: ", error);
-    };
-  };
-
+  const [showDetails, setShowDetails] = useState(true);
   const [add, setAdd] = useState(false);
   const [formData, setFormData] = useState({
     title: "",
@@ -81,118 +71,139 @@ const WriteBook = ({ close }) => {
         </button>
         <h1>Write a book</h1>
         <form onSubmit={(e) => (add ? addChapter(e) : onSubmit(e))}>
-          <div className="d-flex justify-content-between">
-            <span>
-              <label>title</label>
-              <input
-                name="title"
-                value={title}
-                onChange={(e) => onChange(e)}
-                readOnly={add}
-                required
-              />
-            </span>
-            <span>
-              <label>author name</label>
-              <input
-                name="author"
-                value={AuthStore.auth.user.name}
-                readOnly
-                required
-              />
-            </span>
-          </div>
-
-          <div className="d-flex justify-content-between">
-            <span>
-              <select
-                onChange={(e) =>
-                  setFormData({ ...formData, genre: e.target.value })
-                }>
-                {BookStore.state.genres.map((x, i) => (
-                  <option key={i} value={x.genre}>
-                    {x.genre}
-                  </option>
-                ))}
-              </select>
-            </span>
-            <span>
-              <div className="img">
-                <img
-                  alt="file"
-                  src={process.env.PUBLIC_URL + "/images/file.png"}
-                />
-                <p>{picture.name ? picture.name : "Select"}</p>
-                <label>
-                  Upload
+          {showDetails ? (
+            <>
+              <div className="d-flex justify-content-between">
+                <span>
+                  <label>title</label>
                   <input
-                    type="file"
-                    // onChange={(e) => {
-                    //   getBase64(e.target.files[0], (result) => {
-                    //     console.log(result);
-                    //     setFormData({ ...formData, imgsrc: result });
-                    //   });
-                    // }}
-                    onChange={(e) => {
-                      console.log(e.target.files[0]);
-                      setFormData({ ...formData, picture: e.target.files[0] });
-                    }}
+                    name="title"
+                    value={title}
+                    onChange={(e) => onChange(e)}
+                    readOnly={add}
+                    required
                   />
-                </label>
+                </span>
+                <span>
+                  <label>author name</label>
+                  <input
+                    name="author"
+                    value={AuthStore.auth.user.name}
+                    readOnly
+                    required
+                  />
+                </span>
               </div>
-            </span>
-          </div>
 
-          <label>story</label>
-          <div className="story">
-            <button
-              className="add-chapter"
-              onClick={(e) => {
-                setFormData({
-                  ...formData,
-                  content: EditorState.createEmpty(),
-                });
-                if (!add) {
-                  BookStore.createBook({
-                    ...formData,
-                    content: draftToHtml(
-                      convertToRaw(content.getCurrentContent()),
-                    ),
-                  });
-                  setAdd(true);
-                } else {
-                  addChapter(e);
-                }
-              }}>
-              <img
-                alt="add"
-                src={process.env.PUBLIC_URL + "/images/addchapter.png"}
-              />
-            </button>
-            {/* <textarea name="content" value={content} onChange={onChange} /> */}
-
-            <Editor
-              onEditorStateChange={(e) =>
-                setFormData({
-                  ...formData,
-                  content: e,
-                })
-              }
-            />
-
-            <div className="d-flex justify-content-center">
-              <Link
-                target="_blank"
-                className="btn btn-primary btn-green m-1"
-                to="/story/proofread"
-                onClick={(e) => onProofRead(e)}>
-                Proof Read
-              </Link>
-              <button type="submit" className="btn btn-primary btn-purple m-1">
-                Published
+              <div className="d-flex justify-content-between">
+                <span>
+                  <select
+                    onChange={(e) =>
+                      setFormData({ ...formData, genre: e.target.value })
+                    }>
+                    {BookStore.state.genres.map((x, i) => (
+                      <option key={i} value={x.genre}>
+                        {x.genre}
+                      </option>
+                    ))}
+                  </select>
+                </span>
+                <span>
+                  <div className="img">
+                    <img
+                      alt="file"
+                      src={process.env.PUBLIC_URL + "/images/file.png"}
+                    />
+                    <p>{picture.name ? picture.name : "Select"}</p>
+                    <label>
+                      Upload
+                      <input
+                        type="file"
+                        // onChange={(e) => {
+                        //   getBase64(e.target.files[0], (result) => {
+                        //     console.log(result);
+                        //     setFormData({ ...formData, imgsrc: result });
+                        //   });
+                        // }}
+                        onChange={(e) => {
+                          console.log(e.target.files[0]);
+                          setFormData({
+                            ...formData,
+                            picture: e.target.files[0],
+                          });
+                        }}
+                      />
+                    </label>
+                  </div>
+                </span>
+              </div>
+              <button
+                className="btn btn-success mt-3"
+                onClick={() => setShowDetails(false)}>
+                Next
               </button>
-            </div>
-          </div>
+            </>
+          ) : (
+            <>
+              <label>story</label>
+              <div className="story">
+                <button
+                  className="add-chapter"
+                  onClick={(e) => {
+                    setFormData({
+                      ...formData,
+                      content: EditorState.createEmpty(),
+                    });
+                    if (!add) {
+                      BookStore.createBook({
+                        ...formData,
+                        content: draftToHtml(
+                          convertToRaw(content.getCurrentContent()),
+                        ),
+                      });
+                      setAdd(true);
+                    } else {
+                      addChapter(e);
+                    }
+                  }}>
+                  <img
+                    alt="add"
+                    src={process.env.PUBLIC_URL + "/images/addchapter.png"}
+                  />
+                </button>
+                {/* <textarea name="content" value={content} onChange={onChange} /> */}
+
+                <Editor
+                  onEditorStateChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      content: e,
+                    })
+                  }
+                />
+
+                <div className="d-flex justify-content-center">
+                  <Link
+                    target="_blank"
+                    className="btn btn-primary btn-green m-1"
+                    to="/story/proofread"
+                    onClick={(e) => onProofRead(e)}>
+                    Proof Read
+                  </Link>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-purple m-1">
+                    Published
+                  </button>
+                </div>
+              </div>
+              <button
+                className="btn btn-success mt-3"
+                onClick={() => setShowDetails(true)}>
+                Go Back
+              </button>
+            </>
+          )}
         </form>
       </div>
     </div>
--- src/components/Story/WriteStory.js
+++ src/components/Story/WriteStory.js
@@ -9,16 +9,7 @@ import draftToHtml from "draftjs-to-html";
 import { convertToRaw } from "draft-js";
 
 const WriteStory = ({ close }) => {
-  const getBase64 = (file, cb) => {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-      cb(reader.result);
-    };
-    reader.onerror = function (error) {
-      console.log("Error: ", error);
-    };
-  };
+  const [showDetails, setShowDetails] = useState(true);
 
   const [formData, setFormData] = useState({
     title: "",
@@ -70,114 +61,114 @@ const WriteStory = ({ close }) => {
         </button>
         <h1>Write a story</h1>
         <form onSubmit={(e) => onSubmit(e)}>
-          <div className="d-flex justify-content-between">
-            <span>
-              <label>title</label>
-              <input name="title" value={title} onChange={onChange} required />
-            </span>
-            <span>
-              <label>author name</label>
-              <input
-                name="author"
-                value={AuthStore.auth.user.name}
-                readOnly
-                required
-              />
-            </span>
-          </div>
-
-          <div className="d-flex justify-content-between">
-            <span>
-              <select
-                onChange={(e) =>
-                  setFormData({ ...formData, genre: e.target.value })
-                }>
-                {BookStore.state.genres.map((x, i) => (
-                  <option key={i} value={x.genre}>
-                    {x.genre}
-                  </option>
-                ))}
-              </select>
-            </span>
-            <span>
-              <div className="img">
-                <img
-                  alt="file"
-                  src={process.env.PUBLIC_URL + "/images/file.png"}
-                />
-                <p>{picture.name ? picture.name : "Select"}</p>
-                <label>
-                  Upload
+          {showDetails ? (
+            <>
+              {" "}
+              <div className="d-flex justify-content-between">
+                <span>
+                  <label>title</label>
                   <input
-                    type="file"
-                    // onChange={(e) => {
-                    //   getBase64(e.target.files[0], (result) => {
-                    //     console.log(result);
-                    //     setFormData({ ...formData, imgsrc: result });
-                    //   });
-                    // }}
-                    onChange={(e) => {
-                      console.log(e.target.files[0]);
-                      setFormData({ ...formData, picture: e.target.files[0] });
-                    }}
+                    name="title"
+                    value={title}
+                    onChange={onChange}
+                    required
                   />
-                </label>
+                </span>
+                <span>
+                  <label>author name</label>
+                  <input
+                    name="author"
+                    value={AuthStore.auth.user.name}
+                    readOnly
+                    required
+                  />
+                </span>
               </div>
-            </span>
-          </div>
-
-          <label>story</label>
-          <div className="story">
-            {/* <textarea
-              name="content"
-              value={content}
-              onChange={onChange}
-              required
-            /> */}
-            {/* <RichTextEditor
-              toolbarConfig={toolbarConfig}
-              value={content}
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  content: e.target.value.toString("html"),
-                })
-              }
-            /> */}
-
-            <Editor
-              onEditorStateChange={(e) =>
-                setFormData({
-                  ...formData,
-                  content: e,
-                })
-              }
-              toolbar={{
-                inline: { inDropdown: true },
-                list: { inDropdown: true },
-                textAlign: { inDropdown: true },
-                link: { inDropdown: true },
-                history: { inDropdown: true },
-                // image: {
-                //   uploadCallback: uploadImageCallBack,
-                //   alt: { present: true, mandatory: true },
-                // },
-              }}
-            />
-
-            <div className="d-flex justify-content-center">
-              <Link
-                target="_blank"
-                className="btn btn-green m-1"
-                to="/story/proofread"
-                onClick={() => onProofRead()}>
-                Proof Read
-              </Link>
-              <button type="submit" className="btn btn-primary btn-purple m-1">
-                Published
+              <div className="d-flex justify-content-between">
+                <span>
+                  <select
+                    onChange={(e) =>
+                      setFormData({ ...formData, genre: e.target.value })
+                    }>
+                    {BookStore.state.genres.map((x, i) => (
+                      <option key={i} value={x.genre}>
+                        {x.genre}
+                      </option>
+                    ))}
+                  </select>
+                </span>
+                <span>
+                  <div className="img">
+                    <img
+                      alt="file"
+                      src={process.env.PUBLIC_URL + "/images/file.png"}
+                    />
+                    <p>{picture.name ? picture.name : "Select"}</p>
+                    <label>
+                      Upload
+                      <input
+                        type="file"
+                        onChange={(e) => {
+                          console.log(e.target.files[0]);
+                          setFormData({
+                            ...formData,
+                            picture: e.target.files[0],
+                          });
+                        }}
+                      />
+                    </label>
+                  </div>
+                </span>
+              </div>
+              <button
+                className="btn btn-success mt-3"
+                onClick={() => setShowDetails(false)}>
+                Next
               </button>
-            </div>
-          </div>
+            </>
+          ) : (
+            <>
+              {" "}
+              <label>story</label>
+              <div className="story">
+                <Editor
+                  onEditorStateChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      content: e,
+                    })
+                  }
+                  toolbar={{
+                    inline: { inDropdown: true },
+                    list: { inDropdown: true },
+                    textAlign: { inDropdown: true },
+                    link: { inDropdown: true },
+                    history: { inDropdown: true },
+                  }}
+                />
+
+                <div className="d-flex justify-content-center">
+                  <Link
+                    target="_blank"
+                    className="btn btn-green m-1"
+                    to="/story/proofread"
+                    onClick={() => onProofRead()}>
+                    Proof Read
+                  </Link>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-purple m-1">
+                    Published
+                  </button>
+                </div>
+              </div>{" "}
+              <button
+                className="btn btn-success mt-3"
+                onClick={() => setShowDetails(true)}>
+                Go Back
+              </button>
+            </>
+          )}
         </form>
       </div>
     </div>
